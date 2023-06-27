7499f84e0eef54edeb454f5766808754d63ef6eb
Marek Kloczewiak <marek.kloczewiak@linktransport.eu>
2023-06-27 15:56:22 +0200

    added: auth module users module guard
    	new file:   src/auth/auth.service.spec.ts

---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'di[afdsa fe433w2f eaw ].',
+  };
---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+
+export type User = any;
+
+@Injectable()
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      country: 'poland',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      country: 'usa',
+      password: 'guess',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find(user => user.username === username);
+  }
+}
